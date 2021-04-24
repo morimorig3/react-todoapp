b901136e5bc59ebb127faff5bff78e0b118de420
morimorig3 <mori.nto@outlook.jp>
2021-04-24 18:54:53 +0900

    split Todolist.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FaGithub } from 'react-icons/fa'
+import Todolist from './Todolist'
 
 const APP_NAME = 'TODO_APP!'
 
 const TodoApp = () => {
-    const styleS = {
-        color:'#cccccc'
-    }
     const styleGithub = {
         color:'#000000',
         fontSize:'30px'
@@ -42,20 +40,13 @@ const TodoApp = () => {
         setTask('')
         setdisabled(true)
     }
-    const deleteTask = (index) => {
-        const newTask = [...todo]
-        newTask.splice(index, 1)
-        setTodo(newTask)
-    }
-    const completedTask = (index) => {
-        const newTask = [...todo]
-        newTask[index].isCompleted = !newTask[index].isCompleted
-        setTodo(newTask)
-    }
     return (
         <Container className="py-sm-4 py-2">
             <Row>
-                <Col className='d-flex justify-content-between'><h1 className="font-weight-bold">TO DO APP</h1><a style={styleGithub} href='https://github.com/morimorig3/react-todoapp' target='_blank' rel="noreferrer"><FaGithub /></a></Col>
+                <Col className='d-flex justify-content-between'>
+                    <h1 className="font-weight-bold">TO DO APP</h1>
+                    <a style={styleGithub} href='https://github.com/morimorig3/react-todoapp' target='_blank' rel="noreferrer"><FaGithub /></a>
+                    </Col>
             </Row>
             <Row className="mb-3">
                 <Col>
@@ -82,30 +73,7 @@ const TodoApp = () => {
             </Row>
             <Row>
                 <Col>
-                    <ul className="p-0">
-                        {todo.map((todo, index) => {
-                            return (
-                                <li className="d-flex align-items-center border p-2 mb-2 rounded" key={index}>
-                                <span className="col-10 text-break form-check">
-                                    <input
-                                        className="form-check-input"
-                                        type="checkbox"
-                                        value=""
-                                        id={`check-${index}`}
-                                        checked={todo.isCompleted}
-                                        onChange={() => completedTask(index)}
-                                    />
-                                    <label className="form-check-label" htmlFor={`check-${index}`}>
-                                        {todo.isCompleted ? <s style={styleS}>{todo.task}</s> : todo.task}
-                                    </label>
-                                </span>
-                                <div className="col-2 d-flex align-items-center justify-content-center">
-                                    <Button onClick={() => deleteTask(index)} type="button" variant="danger">×</Button>
-                                </div>
-                            </li>
-                            )
-                        })}
-                    </ul>
+                    <Todolist todo={todo} setTodo={setTodo} />
                 </Col>
             </Row>
         </Container>
